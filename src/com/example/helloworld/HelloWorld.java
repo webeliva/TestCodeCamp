62a6f7b35b79c977d194d4f1bc55b34595faeb06
webeliva <kencompute@gmail.com>
2022-09-07 21:24:00 +0100

    First_Java Project

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello Hiring Manager."
+                +"\n I am submitting my resume document for the position of  Quality Assurance Engineer."
+                +"\n As a skilled and highly educated professional with years of experience testing Mobile & Web-based applications."
+                +"\n I am confident of my ability to make a significant contribution to your organization."
+                +"\n My professional job functions include analyzing the business requirements, creating test strategies"
+                +"\n and executing test cases to identify bugs in new software while creating and maintaining automation packs for the same."
+                +"\n With my educational background and my attention to detail and performance,"
+                +"\n I am in an excellent position to help your organization to produce top-quality software products"
+                +"\n The following are some Skills and highlights of my qualifications and experience:"
+                +"\n ●\t Working Knowledge of tools like JIRA,"
+                +"\n ●\t Experience in Jenkins"
+                +"\n ●\t UI Testing"
+                +"\n ●\t Experienced all phases of  Agile and Waterfall models"
+                +"\n My keen knowledge of software testing, review, and validation—along with my ability to think of innovative solutions and "
+                +"\n present them to clients—will allow me to bring much value to your organization. I look forward to discussing this position and my qualifications with you."
+
+                +"\n Thank you for your consideration"
+
+                +"\n Regards,"
+                +"\n Kelvin Ojo");
+        
+    }
+
+}
